6608f43e6f18661637c5d4ab88959a3675e9ca4c
ulvixz <aliulvi4103@hotmail.com>
2026-09-23 17:18:04 +0200

    updated frame to match with sim

--- libraries/AP_Motors/AP_Motors6DOF.cpp
+++ libraries/AP_Motors/AP_Motors6DOF.cpp
@@ -177,12 +177,12 @@ void AP_Motors6DOF::setup_motors(motor_frame_class frame_class, motor_frame_type
 
     case SUB_FRAME_CUSTOM:
         _frame_class_string = "ASIA MADELEINE";
-        add_motor_raw_6dof(AP_MOTORS_MOT_1,     1.0f,              1.0f,              0,           1.0f,                  0,              0,           1);
+        add_motor_raw_6dof(AP_MOTORS_MOT_1,     -1.0f,              -1.0f,              0,           1.0f,                  0,              0,           1);
         add_motor_raw_6dof(AP_MOTORS_MOT_2,     0,              0,              1.0f,          0,                  1.0f,              0,          2);
-        add_motor_raw_6dof(AP_MOTORS_MOT_3,     1.0f,              -1.0f,              0,          1.0f,                  0,               0,           3);
+        add_motor_raw_6dof(AP_MOTORS_MOT_3,     -1.0f,              1.0f,              0,          1.0f,                  0,               0,           3);
         add_motor_raw_6dof(AP_MOTORS_MOT_4,     0,              0,              -1.0f,           0,                  1.0f,               0,          4);
-        add_motor_raw_6dof(AP_MOTORS_MOT_5,     -1.0f,           1.0f,              0,              1.0f,              0,                  0,              5);
-        add_motor_raw_6dof(AP_MOTORS_MOT_6,     -1.0f,          -1.0f,              0,              1.0f,              0,                  0,              6);
+        add_motor_raw_6dof(AP_MOTORS_MOT_5,     1.0f,           -1.0f,              0,              1.0f,              0,                  0,              5);
+        add_motor_raw_6dof(AP_MOTORS_MOT_6,     1.0f,          1.0f,              0,              1.0f,              0,                  0,              6);
         break;
 
     case SUB_FRAME_SIMPLEROV_3:
